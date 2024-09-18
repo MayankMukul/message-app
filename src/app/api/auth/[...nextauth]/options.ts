73917de369e5f5083c49e086bd1bd3cef8d0b0b0
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-09-18 22:44:32 +0530

    next auth options

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,21 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcryptjs';
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+export const authOptions:NextAuthOptions = {
+    providers : [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                username: { label: "Email", type: "text" },
+                password: { label: "Password", type:"password"}
+            },
+            async authorize(credentials){
+                
+            }
+        })
+    ]
+}
