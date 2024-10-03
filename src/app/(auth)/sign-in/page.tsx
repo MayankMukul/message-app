9fbf372db72a9b644afd6fc61ba11ad9e8370b85
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-10-03 22:46:59 +0530

    check unique username

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,11 +3,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import Link from 'next/link';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounceValue } from 'usehooks-ts';
 import { useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/router";
-
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios from 'axios';
 
 
 export default function page() {
@@ -20,8 +21,34 @@ export default function page() {
   const {toast } = useToast();
   const router = useRouter();
 
-  
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues:{
+      username: '',
+      email: '',
+      password: '',
+    }
+  })
 
+  useEffect(()=>{
+    const checkUsernameUnique = async ()=>{
+      if(debouncedUsername){
+        setIsCheckingUsername(true);
+        setUsernameMessage('');
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`);
+          setUsernameMessage(response.data.message)
+        } catch (error) {
+          console.log("Error", error);
+        }finally{
+          setIsCheckingUsername(false);
+        }
+      }
+    }
+    checkUsernameUnique();
+  },[debouncedUsername])
+
+  
   return (
     <div>page</div>
   )
