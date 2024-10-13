57e6b5543b3039acf99b94c306a4c95f7e593de3
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-10-13 23:24:15 +0530

    verify page update

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,6 +1,11 @@
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import { verifySchema } from '@/schemas/verifySchema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
 import { useParams } from 'next/navigation';
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -15,7 +20,60 @@ export default function VerifyAccount() {
     const form = useForm<z.infer<typeof verifySchema>>({
         resolver: zodResolver(verifySchema),
     })
+
+    const onSubmit = async (data: z.infer<typeof verifySchema>)=>{
+      try {
+        const response = await axios.post('/api/verify-code',{
+          username: param.username,
+          code: data.code
+        })
+
+        toast({
+          title: 'success',
+          description: response.data.message
+        })
+        router.replace('sign-in');
+      } catch (error) { 
+        console.log("error",error);
+
+        toast({
+          title: 'Signup Failed',
+          
+        })
+        
+      }
+    }
   return (
-    <div>VerifyAccount</div>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className="w-full max-w-nd p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className='test-4xl font-extrabold tracking-tight lg:text05xl mb-6'>
+            Verify Your Account
+          </h1>
+          <p className='mb-4'>Enter the verification code sent to your email</p>
+        </div>
+
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="code"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Verification Code</FormLabel>
+              <FormControl>
+                <Input placeholder="code" {...field} />
+              </FormControl>
+             
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  
+      </div>
+    </div>
   )
 }
